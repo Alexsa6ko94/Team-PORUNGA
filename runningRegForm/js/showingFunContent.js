e44e100a837c49b75ea8b78fad5d751297a490c5
Kibork4o <kibork4o@gmail.com>
2014-06-21 18:57:38 +0300

    Create showingFunContent.js

---
+++ runningRegForm/js/showingFunContent.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	//funkciq, proverqvashta, dali e imalo 3 minavaniq s ishkata prez formata za registraciq
+	function howMuchTurns(){
+		var count=$(".mouseTurns").val();
+		count=parseInt(count);
+		$(".funChanger").text($(".mouseTurns").val());
+		if(count>3){
+			funChanger($(window).width(), $(window).height());
+		}
+	}
+	//kogato e vreme pokazvame smeshnoto sadarjanie
+	function funChanger(width, height){
+		$(".funContent").animate({width: width + "px", height: height + "px"}, 2000);
+		$(".funContent").css({background: "black", opacity: "0.7"});
+		$(".closeFunContent").css({visibility: "visible"});
+		//pravim overflow hidden, za da mahnem scrolla dokato imame zatamnen ekran
+		$("body").css({overflow:"hidden"});
+		zanulitel();
+	}
+	//funkciq, zanulitel - maha forata za registraciq i zanulqva poleto za poqvqvane na zatamnenieto
+	function zanulitel(){
+		$(".regForm").hide();
+		$(".mouseTurns").val(0);
+	}
+	//funkciq, zakrivashta zatamnenoto pole pri klick varhu spana cross
+	function closeFunChanger(){	
+		$(".closeFunContent").css({visibility: "hidden"});
+		$(".funContent").animate({width: "0px", height: "0px"}, 2000);
+		setTimeout(
+			function(){
+				$(".funContent").css({background: "white", opacity: "0"});
+				$("body").css({overflow: "auto"});
+				$(".regForm").show();
+		}, 2000);
+	}
+	//deistvame
+	$(".regForm").on("mouseover", function(){
+		howMuchTurns();
+	});
+	$(".closeFunContent").on("click", function(){
+		closeFunChanger();
+	});
+});
