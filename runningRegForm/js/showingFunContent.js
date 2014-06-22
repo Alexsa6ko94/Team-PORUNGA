a3d618e299ff57e208eb3ccc37362ffb76c5b610
Kibork4o <kibork4o@gmail.com>
2014-06-23 01:19:39 +0300

    Update showingFunContent.js

--- runningRegForm/js/showingFunContent.js
+++ runningRegForm/js/showingFunContent.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
 	//funkciq, proverqvashta, dali e imalo 3 minavaniq s ishkata prez formata za registraciq
-	function howMuchTurns(){
+	//var resize - "true": orazmerqvame prozoreca, "false": minali sme s mihkata prez formata za registraciq
+	function howMuchTurns(resize){
 		var count=$(".mouseTurns").val();
 		count=parseInt(count);
-		$(".funChanger").text($(".mouseTurns").val());
 		if(count>3){
-			funChanger($(window).width(), $(window).height());
+			if(resize==false){
+				funChanger($(window).width(), $(window).height());
+			}
+			else{
+				funChangerResize($(window).width(), $(window).height());
+			}
 		}
 	}
 	//kogato e vreme pokazvame smeshnoto sadarjanie
@@ -17,10 +22,13 @@ $(document).ready(function(){
 		$("body").css({overflow:"hidden"});
 		zanulitel();
 	}
+	//pri resize preorazmerqvame 4ernoto pole
+	function funChangerResize(width, height){
+		$(".funContent").animate({width: width + "px", height: height + "px"}, 0);
+	}
 	//funkciq, zanulitel - maha forata za registraciq i zanulqva poleto za poqvqvane na zatamnenieto
 	function zanulitel(){
 		$(".regForm").hide();
-		$(".mouseTurns").val(0);
 	}
 	//funkciq, zakrivashta zatamnenoto pole pri klick varhu spana cross
 	function closeFunChanger(){	
@@ -28,14 +36,18 @@ $(document).ready(function(){
 		$(".funContent").animate({width: "0px", height: "0px"}, 2000);
 		setTimeout(
 			function(){
-				$(".funContent").css({background: "rgba(255, 255, 255, 0)"});
+				$(".funContent").css({background: "rgba(255, 255, 255, 0.0)"});
 				$("body").css({overflow: "auto"});
 				$(".regForm").show();
 		}, 2000);
+		$(".mouseTurns").val(0);
 	}
 	//deistvame
 	$(".regForm").on("mouseover", function(){
-		howMuchTurns();
+		howMuchTurns(false);
+	});
+	$(window).on("resize", function(){
+		howMuchTurns(true);
 	});
 	$(".closeFunContent").on("click", function(){
 		closeFunChanger();
