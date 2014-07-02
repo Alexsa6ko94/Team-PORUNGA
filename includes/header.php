6c55cbc0732a02fb11370fe47367008502efe9bc
dzhabrailov <dzhabrailov@abv.bg>
2014-07-03 01:47:36 +0300

    fix logo

--- includes/header.php
+++ includes/header.php
@@ -10,6 +10,7 @@
 					<span class="icon-bar"></span>
 				  </button>
 				  <a class="navbar-brand" href="#">TEAM PORUNGA</a>
+				  <img alt="" src="./Images/sm_logo.png">
 				</div>
 				<div class="collapse navbar-collapse">
 				  <ul class="nav navbar-nav">
