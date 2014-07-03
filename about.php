83a2b3abe351fb3d939a3e30a057e6920c24c1cf
dzhabrailov <dzhabrailov@abv.bg>
2014-07-03 16:42:36 +0300

    about

--- about.php
+++ about.php
@@ -1,53 +1,65 @@
 <!DOCTYPE html>
-<html>
-<head lang="en">
-    <meta charset="UTF-8">
-    <title>About us</title>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="The funny side of Software University">
-    <meta name="author" content="Alexandar Boyanov">
-    <link rel="shortcut icon" href="Images/favicon1.ico">
-    <link href="Styles/bootstrap.min.css" rel="stylesheet">
-    <link href="Styles/sticky-footer-navbar.css" rel="stylesheet">
-    <style type="text/css"></style>
-    <!--[if lt IE 9]>
-    <script src="html5shiv.js"></script>
-    <![endif]-->
+<html lang="en">
+<head>
+<?php 
+	include ('includes/metas.php');
+	include ('includes/css.php');
+?>    
+	<!--[if lt IE 9]>
+	<script src="html5shiv.js"></script>
+	<![endif]-->
+	<title>Super Uni</title>
 </head>
 <body>
 <?php 
-    include ('includes/header.php');
+	include ('includes/header.php');
 ?>
-            <!-- Begin page content -->
-    <div id="container">
-    <div class="page-header">
-        <h1>SuperSoftUni</h1>
-          </div>
-        <p class="lead">About us</p>
-        <section class="col-sm-4">
-            <div>
-              <h2>Moryk 1</h2>
-            </div>
-        <p>ala bala </p>
-        </section>
-        <section class="col-sm-4">
-            <div>
-              <h2>Moryk 2</h2>
-            </div>
-        <p>ala bala </p>
-        </section>
-        <section class="col-sm-4">
-            <div>
-              <h2>Moryk 3</h2>
-            </div>
-        <p>ala bala </p>
-        </section>
+    <!-- Begin page content -->
+    <div class="container mainContent">
+		  <div class="page-header">
+			<h1>The "Dream Team"</h1>
+		  </div>
+			<section class="col-sm-3">
+			<div>
+				<h3>Александър Боянов</h3>
+				<p>Трудно описуема личност - истинска "майна"! Би било добре да не се занимавате с него...Троши всякакъв код с лекота!</p>
+				<div class="img">
+					<img width="50%" alt="" src="./Images/sasho.png">
+				</div>
+			</div>
+			</section>
+			<section class="col-sm-3">
+			<div>
+				<h3>Станьо Джабраилов</h3>
+				<p>Просто се правете, че не сте го видяли. Photoshop е толкова чужд за него, колкото на г-н Наков английския...</p>
+				<div class="img">
+					<img width="50%" alt="" src="./Images/stanyo.png">
+				</div>
+			</div>
+			</section>
+			<section class="col-sm-3">
+			<div>
+				<h3>Димитър Бонев</h3>
+				<p>JavaScript Master of disaster. Бъговете са присъщи за стилът му! Попаднете ли на негов код "Shif + delete"! <br />Не се колеайте! </p>
+				<div class="img">
+					<img width="50%" alt="" src="./Images/mitaka.png">
+				</div>
+			</div>		
+			</section>
+			<section class="col-sm-3">
+				<div>
+					<h3>Мартин Борисов</h3>
+					<p>Човекът идея! Ако Ви попита имате ли нужда от идея - бъдете предпазливи и се отдалечете на безпоасно разстояние!</p>
+					<div class="img">
+						<img width="50%" alt="" src="./Images/marto.png">
+					</div>
+				</div>
+			</section>
     </div>
-            <!-- End of page content -->
-<?php 
-    include ('bootstrap/loadBootstrap.php');
-    include ('includes/footer.php');
+	
+<?php
+	include ('bootstrap/loadBootstrap.php');
+	include ('includes/footer.php');
 ?>
 </body>
 </html>
